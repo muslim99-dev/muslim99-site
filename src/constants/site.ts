8d3fba85e94c28375a3d8a95b136f94ebab66a9c
Talha Ghauri <talhaghauridev@gmail.com>
2026-01-30 18:46:42 +0500

    add site.ts

---
+++ src/constants/site.ts
@@ -0,0 +1,66 @@
+export const SITE_CONFIG = {
+  name: 'Muslim99',
+  title: 'Muslim99 - Your Complete Islamic Companion',
+  description:
+    'Experience accurate prayer times, Quran recitation, Qibla direction, and Islamic knowledge all in one beautiful app. Stay connected with your faith, anytime, anywhere.',
+  url: 'https://www.themuslim99.com/',
+  ogImage: '/og-image.png',
+  author: 'Muslim99 Team',
+
+  creator: {
+    name: 'Talha Ghauri',
+    email: 'talhaghauridev@gmail.com',
+    image: 'https://talhaghauri.vercel.app/images/talha-ghauri.png',
+    url: 'https://talhaghauri.vercel.app',
+  },
+
+  keywords: [
+    'Muslim99',
+    'Islamic app',
+    'Prayer times',
+    'Salah times',
+    'Quran app',
+    'Qibla direction',
+    'Islamic calendar',
+    'Hijri calendar',
+    'Muslim prayer app',
+    'Adhan',
+    'Azan',
+    'Quran recitation',
+    'Quran translation',
+    'Daily Ayat',
+    'Islamic duas',
+    'Muslim app',
+    'Ramadan app',
+    'Prayer reminder',
+    'Namaz timings',
+    'Islamic knowledge',
+    'Hadith',
+    'Muslim companion',
+    'Prayer notification',
+    'Qibla compass',
+    'Muslim lifestyle',
+  ],
+
+  links: {
+    playStore: 'https://play.google.com/store/apps/details?id=com.muslim99',
+    // Add these when ready:
+    // appStore: 'https://apps.apple.com/app/muslim99/idXXXXXXXXX',
+    // huaweiAppGallery: 'https://appgallery.huawei.com/app/muslim99',
+  },
+
+  contact: {
+    email: 'social@themuslim99.com',
+    supportEmail: 'social@themuslim99.com',
+    website: 'https://themuslim99.com',
+  },
+
+  social: {
+    facebook: 'https://facebook.com',
+    instagram: 'https://instagram.com',
+    twitter: 'https://twitter.com',
+    youtube: 'https://youtube.com',
+    tiktok: 'https://tiktok.com',
+    linkedin: 'https://linkedin.com',
+  },
+};
